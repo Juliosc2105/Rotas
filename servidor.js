32bf28237b33bcfc8679572de81fdba43372987b
Julio Cardoso <juliocesarsc2008@gmail.com>
2021-09-29 16:13:52 -0300

    to puto

---
+++ servidor.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require ("express")
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/style.css')
+})
+
+app.get('/teste.js', function(req, res){
+    res.sendFile(__dirname + '/teste.js')
+})
+
+app.get('/script.js', function(req, res){
+    res.sendFile(__dirname + '/script.js')
+})
+
+
+app.listen(3000)
+console.log("Servido aberto, Link: localhost:3000")
--- teste.js
+++ teste.js
@@ -1,17 +1,25 @@
-var primeiro = []
+const puppeteer = require('puppeteer');
 
-
-for (var i = 0; i < 5; i++)
+export default async function Distancia(foco, interesse)
 {
-    primeiro[i] = new Array;
+    const browser = await puppeteer.launch({headless: true, });
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com.br/maps/dir///@-21.2144642,-50.3998166,15z/data=!4m2!4m1!3e0', {waitUntil: 'domcontentloaded'});
+    await page.waitForSelector('#sb_ifc50')
+    await page.type ('#sb_ifc50', `${endereco[foco]}`)
+    await page.keyboard.press("Tab")
+    await page.keyboard.press("Tab")
+    await page.type ('div#sb_ifc51', `${endereco[interesse]}`)
+    await page.keyboard.press("Enter", {waitUntil: 'networkidle2'})
+    await page.waitForSelector("div.xB1mrd-T3iPGc-trip-tUvA6e > div")
+    distancia[IdDistancia(foco, interesse)] = await page.$eval("div.xB1mrd-T3iPGc-trip-tUvA6e > div", (el) => el.innerHTML)
+    await page.close()  
+    distancia[IdDistancia(foco, interesse)] = ConverterParaNumero(distancia[IdDistancia(foco, interesse)])
+    console.log(`A distância entre ${endereco[foco]} e ${endereco[interesse]} é de ${distancia[IdDistancia(foco, interesse)]} km`)
+    cerebro(foco, interesse)
 }
 
-primeiro[0][0] = 'a'
-primeiro[0][1] = 'b'
-primeiro[4][0] = 'um'
-primeiro[4][1] = 'um'
-
-
-console.log(primeiro)
-console.log(primeiro[0])
-console.log(primeiro[primeiro.length-1].length = 3)
+function IdDistancia (primeiro, segundo)
+{
+    return Number.parseFloat((primeiro.toString())+(segundo.toString()))
+}
